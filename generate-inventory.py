e2d1f72c67311d201a285d0f5696dfbc73a73415
root <root@sbcc.tacc.utexas.edu>
2024-07-30 17:43:43 -0500

    ArgParse functions added

--- generate-inventory.py
+++ generate-inventory.py
@@ -7,9 +7,6 @@ import subprocess
 import argparse
 import netifaces as ni
 
-# Global variables
-VPN_interface = 'wg0'
-local_IP = ni.ifaddresses(VPN_interface)[ni.AF_INET][0]['addr']
 
 
 def nmaprun(subnet,outputfile):
@@ -52,20 +49,40 @@ def generate_host_file(peerlist, hostfile):
         file.write(template.expandtabs(4))
         
 
-def main():
+def main(args):
 
-    subnet = '10.10.10.0/24'
+    VPN_interface = args.interface
+    subnet = args.subnet
     nmapresult = './nmap-test.xml'
-    hostfile = 'hostfile.example'
+    hostfile = '/etc/hosts'
+    inventory = args.inventory
 
-    # nmap over specified subnet, comment out to make static
-    nmaprun(subnet,nmapresult)
+    # nmap over specified subnet, if not specified
+    if args.no_scan is True:
+        nmaprun(subnet,nmapresult)
 
     # Create dict of peers
     peerlist = parseXML(nmapresult)
 
     #generate_anisble_inventory(peerlist)
-    generate_host_file(peerlist,hostfile)
+
+    # Regenerate /etc/hosts, if specified
+    if args.update_hostfile is True:
+        generate_host_file(peerlist,hostfile)
 
 
-main()
+if __name__ == '__main__':
+    # Global variables
+    VPN_interface = 'wg0'
+    local_IP = ni.ifaddresses(VPN_interface)[ni.AF_INET][0]['addr']
+    
+    # Program arguement parser
+    parser = argparse.ArgumentParser(description='Scans subnet and updates host files')
+    parser.add_argument('-i', '--inventory', help='specify Ansible inventory file to update')
+    parser.add_argument('-n', '--subnet', default='10.10.10.0/24', help='specify nmap subnet (default 10.10.10.0/24)')
+    parser.add_argument('--interface', default='wg0', help='specify VPN interface (default wg0)')
+    parser.add_argument('--update-hostfile', action='store_true', help='update /etc/hosts with the scan results')
+    parser.add_argument('--no-scan', action='store_false', help='run without nmap scan, using previous nmap XML result')
+    args = parser.parse_args()
+
+    main(args)
