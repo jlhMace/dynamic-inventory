5c24b80dc8a2317e5b3776d78ea1ee91b5156735
root <root@sbcc.tacc.utexas.edu>
2024-08-01 11:00:46 -0500

    Creates Ansible inventory with pdu hostname variable

--- generate-inventory.py
+++ generate-inventory.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as ET
 import subprocess
 import argparse
 import netifaces as ni
-
+import os.path
 
 
 def nmaprun(subnet,outputfile):
@@ -33,27 +33,35 @@ def parseXML(xmlfile):
                     peerlist[hostname]= IP
     return peerlist
 
-def generate_ansible_inventory(peerlist):
-    # Creates ansible inventory from list of dicts, and
-    # convert to Ansible-friendly yaml format
-    print(yaml.dump(peerlist))
+def is_valid_file(parser,arg):
+    if not os.path.exists(arg):
+        parser.error(f"The file {arg} does not exist.")
+
+def generate_inventory(peerlist, inventory):
+    # Creates ansible inventory from list of dicts in .ini format
+    template="""[rpi:vars]\nansible_connection=ssh\nansible_ssh_user=admin\n\n[rpi]\n"""
+    for key in peerlist.keys():
+        pdu = "pdu" + key[-2:]
+        template += key + " pdu=" + pdu + "\n"
+    with open(inventory, 'w') as file:
+        file.write(template)
+        print(f"Peers written to Ansible inventory at {inventory}")
 
 def generate_host_file(peerlist, hostfile):
     # Create /etc/hosts file with updated peerlist
-
     template = """127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 \n::1         localhost localhost.localdomain localhost6 localhost6.localdomain6 \n\n# RPi Units\n"""
     for key, value in peerlist.items():
         template += key + "\t\t" + value + "\n"
     # Write to /etc/hosts
     with open(hostfile, 'w') as file:
         file.write(template.expandtabs(4))
-        
+        print(f"Peers written to hostfile at {hostfile}")
 
-def main(args):
+def main():
 
     VPN_interface = args.interface
     subnet = args.subnet
-    nmapresult = './nmap-test.xml'
+    nmapresult = args.output
     hostfile = '/etc/hosts'
     inventory = args.inventory
 
@@ -64,7 +72,8 @@ def main(args):
     # Create dict of peers
     peerlist = parseXML(nmapresult)
 
-    #generate_anisble_inventory(peerlist)
+    # Create Ansible inventory
+    generate_inventory(peerlist, inventory)
 
     # Regenerate /etc/hosts, if specified
     if args.update_hostfile is True:
@@ -76,13 +85,13 @@ if __name__ == '__main__':
     VPN_interface = 'wg0'
     local_IP = ni.ifaddresses(VPN_interface)[ni.AF_INET][0]['addr']
     
-    # Program arguement parser
+    # Program argument parser
     parser = argparse.ArgumentParser(description='Scans subnet and updates host files')
-    parser.add_argument('-i', '--inventory', help='specify Ansible inventory file to update')
+    parser.add_argument('-i', '--inventory', default='/etc/ansible/hosts', help='specify Ansible inventory file to update (default /etc/ansible/hosts.dynamic)')
     parser.add_argument('-n', '--subnet', default='10.10.10.0/24', help='specify nmap subnet (default 10.10.10.0/24)')
+    parser.add_argument('-o', '--output', default='/etc/ansible/dynamic-inventory/nmap-output.xml', help='specify output file for nmap scan')
     parser.add_argument('--interface', default='wg0', help='specify VPN interface (default wg0)')
     parser.add_argument('--update-hostfile', action='store_true', help='update /etc/hosts with the scan results')
     parser.add_argument('--no-scan', action='store_false', help='run without nmap scan, using previous nmap XML result')
     args = parser.parse_args()
-
-    main(args)
+    main()
